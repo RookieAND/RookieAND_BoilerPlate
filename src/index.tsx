5c92fe81cd17af7f14847906ac6b0231e9f00672
RookieAND <gwangin1999@naver.com>
2022-09-20 22:18:00 +0900

    Feat : src 파일 추가, react 관련 tsx 추가
    src 내 index.tsx, App.tsx 파일을 추가하고 eslint 관련 라이브러리를 설치했습니다.

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,47 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
-	const prod = argv.mode === "production";
+  const prod = argv.mode === "production";
 
-	return {
-		mode: prod ? "production" : "development",
-		devtool: prod ? "hidden-source-map" : "eval",
-		entry: "./src/index.tsx",
-		output: {
-			path: path.join(__dirname, "/dist"),
-			filename: "[name].js",
-		},
-		devServer: {
-			port: 3000,
-			hot: true,
-		},
-		resolve: {
-			extensions: [".js", ".jsx", ".ts", ".tsx"],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: ["babel-loader", "ts-loader"],
-				},
-			],
-		},
-		plugins: [
-			new webpack.ProvidePlugin({
-				React: "react",
-			}),
-			new HtmlWebpackPlugin({
-				template: "./public/index.html",
-				minify:
-					process.env.NODE_ENV === "production"
-						? {
-								collapseWhitespace: true, // 빈칸 제거
-								removeComments: true, // 주석 제거
-						  }
-						: false,
-			}),
-			new CleanWebpackPlugin(),
-		],
-	};
+  return {
+    mode: prod ? "production" : "development",
+    devtool: prod ? "hidden-source-map" : "eval",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "[name].js"
+    },
+    devServer: {
+      port: 3000,
+      hot: true
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "ts-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: "react"
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        minify:
+          process.env.NODE_ENV === "production"
+            ? {
+                collapseWhitespace: true, // 빈칸 제거
+                removeComments: true // 주석 제거
+              }
+            : false
+      }),
+      new CleanWebpackPlugin()
+    ]
+  };
 };
