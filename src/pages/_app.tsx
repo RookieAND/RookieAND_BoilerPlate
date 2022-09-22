0d639d74cf67b88dd91da687a92b8cd8a2555763
RookieAND <gwangin1999@naver.com>
2022-09-22 23:35:25 +0900

    Feat : Pretendard woff 폰트 추가
    woff 확장자를 지원하는 고딕 폰트인 Pretendard를 추가하였습니다.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "@styles/globals.css";
+import "@styles/global.css";
 
 import { useState } from "react";
 import { QueryClientProvider, QueryClient } from "react-query";
